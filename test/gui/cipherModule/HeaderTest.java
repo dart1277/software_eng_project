50c1a96b313e6241e65b4adbe1fb96659bb6d3a0
alexsh97 <alexsh997@gmail.com>
2018-05-03 00:37:12 +0200

    UPD HeaderTest

--- test/gui/cipherModule/HeaderTest.java
+++ test/gui/cipherModule/HeaderTest.java
@@ -3,22 +3,47 @@ package gui.cipherModule;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class HeaderTest {
 
-    private static Header _header;
+    private  Header _header;
+    private  static String expectedPadd;
+    private  static Integer inputPadd;
+    private  static String expectedComp;
+    private  static Integer inputComp;
 
-    @BeforeClass
-    public static void setUp() throws Exception {
+    @Before
+    public void setUp() throws Exception {
         _header = new Header("HelpMess","1","000000");
+    }
+
+
+    public HeaderTest(Integer inputPadd, String expectedPadd, Integer inputComp, String expectedComp){
+        this.inputPadd = inputPadd;
+        this.expectedPadd = expectedPadd;
+        this.inputComp = inputComp;
+        this.expectedComp = expectedComp;
+    }
+
+    @Parameters
+    public static Collection<Object[]> paddings(){
+        return Arrays.asList(new Object[][] {{9,"000009", 1, "1"}, {59,"000059", 2, "2"}, {529, "000529", 3, "3"}, {6599, "006599", 4, "4"}, {60500, "060500", 5, "5"}});
+
     }
 
     @Test
     public void setComplexityException(){
         try{
-            new Header("help","22","000000");
+            new Header("help",expectedComp + "0","000000");
             fail("Exception was not thrown");
         }
         catch (IllegalArgumentException e)
@@ -30,7 +55,7 @@ public class HeaderTest {
     @Test
     public void setPaddingException(){
         try{
-            new Header("help","2","0");
+            new Header("help","2",inputPadd.toString());
             fail("Exception was not thrown");
         }
         catch (IllegalArgumentException e)
@@ -45,24 +70,36 @@ public class HeaderTest {
 
         _header.setHelpMessage("NewHelp");
         assertEquals("NewHelp",_header.getHelpMessage());
+
     }
 
     @Test
     public void setComplexity() {
         assertEquals("1",_header.getComplexity());
 
-        _header.setComplexity(2);
-        assertEquals("2",_header.getComplexity());
+        _header.setComplexity(inputComp);
+        assertEquals(expectedComp,_header.getComplexity());
     }
+
+
 
     @Test
     public void setPadding() {
         assertEquals("000000",_header.getRawPadding());
         assertEquals(new Integer("000000"), _header.getPadding());
 
-        _header.setPadding(9);
-        assertEquals("000009", _header.getRawPadding());
-        assertEquals(new Integer("000009"),_header.getPadding());
+        _header.setPadding(inputPadd);
+        assertEquals(expectedPadd, _header.getRawPadding());
+        assertEquals(new Integer(expectedPadd),_header.getPadding());
+    }
+
+    @Test
+    public void toStingTest(){
+
+        _header.setComplexity(inputComp);
+        _header.setPadding(inputPadd);
+        String testString = "Complexity: " + _header.getComplexity() + "\nPadding: " + _header.getPadding() + "\nMSG: " + _header.getHelpMessage();
+        assertEquals(testString, _header.toString());
     }
 
 }
