79f826e3683644a4258ba17aa0a12b18811786b5
alexsh97 <alexsh997@gmail.com>
2018-05-12 22:15:20 +0200

    UPD testing exceptions in call() method

--- test/gui/cipherModule/CryptoTaskTest.java
+++ test/gui/cipherModule/CryptoTaskTest.java
@@ -26,20 +26,35 @@ public class CryptoTaskTest {
     private INotifier freezTest;
     @Mock
     private BiConsumer unfreezTest;
+    @Mock
+    private FileEncryptor fileEn;
     @Before
     public void setUp()
     {
         freezTest = mock(INotifier.class);
         unfreezTest = mock(BiConsumer.class);
-        encryptoTaskTest = new EncryptorTask(mock(FileEncryptor.class), freezTest, unfreezTest);
-        decryptoTaskTest = new EncryptorTask(mock(FileEncryptor.class), mock(INotifier.class), mock(BiConsumer.class));
+        fileEn = mock(FileEncryptor.class);
+
+        encryptoTaskTest = new EncryptorTask(fileEn, freezTest, unfreezTest);
+        decryptoTaskTest = new EncryptorTask(fileEn,freezTest, unfreezTest);
+
+        encryptoTaskTest.add("SRC", "DST");
+        decryptoTaskTest.add("SRC", "DST");
+
+        List<String> suc = new ArrayList<>();
+        List<String> fail = new ArrayList<>();
+        suc.add("OK");
+        fail.add("Not OK");
+
+        encryptoTaskTest.setLists(suc, fail);
+        decryptoTaskTest.setLists(suc, fail);
+
+
     }
 
 
     @Test
     public void add() {
-        encryptoTaskTest.add("SRC", "DST");
-        decryptoTaskTest.add("SRC", "DST");
 
         ArrayList<Pair<String, String>> testList = new ArrayList<>();
         testList.add(new Pair<>("SRC", "DST"));
@@ -59,13 +74,11 @@ public class CryptoTaskTest {
 
     @Test
     public void setLists() {
+
         List<String> suc = new ArrayList<>();
         List<String> fail = new ArrayList<>();
         suc.add("OK");
         fail.add("Not OK");
-
-        encryptoTaskTest.setLists(suc, fail);
-        decryptoTaskTest.setLists(suc, fail);
 
         try {
             Field fieldSuc = CryptoTask.class.getDeclaredField("succList");
@@ -109,14 +122,12 @@ public class CryptoTaskTest {
     }
 
     @Test
-    public void call(){
-        encryptoTaskTest.add("SRC", "DST");
+    public void call() throws IOException, CryptoException {
         List<String> suc = new ArrayList<>();
         List<String> fail = new ArrayList<>();
         suc.add("OK");
         suc.add("SRC");
         fail.add("Not OK");
-        encryptoTaskTest.setLists(suc, fail);
 
         assertNull(encryptoTaskTest.call());
 
@@ -131,6 +142,7 @@ public class CryptoTaskTest {
 
             assertEquals(suc, fieldSuc.get(encryptoTaskTest));
             assertEquals(fail, fieldFail.get(encryptoTaskTest));
+
         }
         catch (NoSuchFieldException | IllegalAccessException e)
         {
@@ -138,4 +150,41 @@ public class CryptoTaskTest {
         }
 
     }
+
+    @Test
+    public void callException() throws IOException, CryptoException
+    {
+
+        List<String> suc = new ArrayList<>();
+        List<String> fail = new ArrayList<>();
+        suc.add("OK");
+        //suc.add("SRC");
+        fail.add("Not OK");
+
+        try {
+            Field fieldSuc = CryptoTask.class.getDeclaredField("succList");
+            Field fieldFail = CryptoTask.class.getDeclaredField("failList");
+            fieldSuc.setAccessible(true);
+            fieldFail.setAccessible(true);
+
+            doThrow(new IOException()).when(fileEn).encrypt("SRC", "DST");
+            encryptoTaskTest.call();
+            fail.add("SRC");
+            assertEquals(suc, fieldSuc.get(encryptoTaskTest));
+            assertEquals(fail, fieldFail.get(encryptoTaskTest));
+
+            doThrow(new CryptoException("")).when(fileEn).encrypt("SRC", "DST");
+            encryptoTaskTest.call();
+            fail.add("SRC");
+            assertEquals(suc, fieldSuc.get(encryptoTaskTest));
+            assertEquals(fail, fieldFail.get(encryptoTaskTest));
+
+        }
+        catch (NoSuchFieldException | IllegalAccessException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
 }
